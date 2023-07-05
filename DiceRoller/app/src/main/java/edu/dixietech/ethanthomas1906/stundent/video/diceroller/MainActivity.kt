71632778734fc8be38dab571ab77b8c64ea6c254
EthanWThomas <111320646+EthanWThomas@users.noreply.github.com>
2023-07-05 16:50:36 -0600

    Lesson 13, 14, 15 and 16 Complete
    Lesson 13: Adding the Button.
    add a button to my activity_main_xml
    
    Lesson 14: Exercise: Adding the Button.
    Lesson 15: Connecting the Button.
    Lesson 16: Exercise: findViewById.

--- DiceRoller/app/src/main/java/edu/dixietech/ethanthomas1906/stundent/video/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/edu/dixietech/ethanthomas1906/stundent/video/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package edu.dixietech.ethanthomas1906.stundent.video.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollbutton: Button = findViewById(R.id.roll_button)
+        rollbutton.text = "Lets Roll!"
     }
 }
 
