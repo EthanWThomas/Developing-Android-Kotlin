793d43eab03157feddcf9a4f5c73cdc621d89223
EthanWThomas <111320646+EthanWThomas@users.noreply.github.com>
2023-08-09 14:35:09 -0600

    Lesson 5: Completed
    Exercise: The Application Class and Timber
    
    1. Add Timber library to gradle file (Module: app)
    
    2. Make an Application class
    
    3. Add the application class to the manifest:
    
    4. Initialize Timber in the application class:
    
    5. Update log statements from previous exercise to timber logs:

--- andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
@@ -153,6 +154,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        Log.i("MainActivity", "OnStart Called")
+        Timber.i("onStart Called")
     }
 }
